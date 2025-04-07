0845dc782b0dd66e98ea059dc9531b36987aed85
Hussein Al Mawla <info@hmawla.com>
2025-04-07 14:50:14 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/direct-chat/'
+      ? '/'
       : '/'
 })
